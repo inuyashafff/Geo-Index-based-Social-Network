2d9fe3c206ac360c56104d1f301b44667322cf10
Jiang Moquan <jiangmoquan@JiangdeMacBook-Pro.local>
2019-04-28 21:41:58 -0400

    remove post authorization

--- around-web/src/constants.js
+++ around-web/src/constants.js
@@ -1,6 +1,6 @@
 
-//export const API_ROOT = 'https://back-end-dot-geo-index-social-network.appspot.com/api/v1';
-export const API_ROOT = 'http://localhost:8080/api/v1';
+export const API_ROOT = 'https://back-end-dot-geo-index-social-network.appspot.com/api/v1';
+//export const API_ROOT = 'http://localhost:8080/api/v1';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const GEO_OPTIONS = {
  enableHighAccuracy: true,
